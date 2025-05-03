20edbc34756c195d4c7c23bbebcd2331e1be3c06
Overmulka <overmulka@gmail.com>
2025-05-04 05:16:46 +0700

    UI Dust

--- Assets/Skripts/PlayerHealth.cs
+++ Assets/Skripts/PlayerHealth.cs
@@ -1,17 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     
     [SerializeField] public static float health = 100;
+    [SerializeField] public static float dustcount = 0;
+    [SerializeField] private TextMeshProUGUI Dust2;
 
+    float ADust = 0;
     public void TakeDamage(float amount)
     {
+        
         health -= amount;
         Debug.Log("Player HP: " + health);
         if (health <= 0)
         {
             // смерть
         }
+    }
+    public void TakeDustAngel(float a) 
+    {
+        ADust += PlayerHealth.dustcount + a;
+        Dust2.text = $"{ADust:0}%";
     }
 }
